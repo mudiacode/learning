faa95573e7dd7dec068c8307354bea1273b4711e
oo378 <o.osagie1@uni.brighton.ac.uk>
2024-10-28 13:35:13 +0000

    add design Pattern > factory method

---
+++ java/design-patterns/factory-method/src/mudia/CreditCardPayment.java
@@ -0,0 +1,9 @@
+package mudia;
+
+import java.math.BigDecimal;
+
+public class CreditCardPayment {
+    void pay(BigDecimal amount) {
+        System.out.println("Paid by credit card!");
+    }
+}
---
+++ java/design-patterns/factory-method/src/mudia/GooglePayPayment.java
@@ -0,0 +1,9 @@
+package mudia;
+
+import java.math.BigDecimal;
+
+public class GooglePayPayment {
+    void pay(BigDecimal amount) {
+        System.out.println("Paid by Google Pay!");
+    }
+}
